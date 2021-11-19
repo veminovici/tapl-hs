4f515abd82ad8be949e6f9a28b5413a2f358a284
veminovici <veminovici@hotmail.com>
2021-11-19 21:25:30 +0200

    Refactor main

--- UnArithmetic/app/Main.hs
+++ UnArithmetic/app/Main.hs
@@ -4,16 +4,18 @@ import Text.Parsec
 import Parser
 import Syntax
 
-peval :: String -> Either ParseError Term
-peval s = eval <$> parse parseTerm "arith" s
+peval :: String -> (String, Either ParseError Term)
+peval s = (s, eval <$> parse parseTerm "arith" s)
 
-prntEval :: String -> IO ()
-prntEval s = putStrLn $ s ++ " => " ++ show (peval s)
+showRes :: (String, Either ParseError Term) -> String
+showRes (s, e) = s ++ " ===> " ++ show e
+
+prnterm :: String -> String
+prnterm = showRes . peval
 
 main :: IO ()
 main = do
-    prntEval "if (if zero? 0 then true else false) then true else false"
-    prntEval "if false then 0 else succ (pred (succ 0))"
-    prntEval "if true then succ 0 else 1"
-    prntEval "if true then true else (if false then false else false)"
-    prntEval "if (zero? (if (zero? 0) then 0 else (succ 0))) then succ 0 else succ (succ 0)"
+    putStrLn $ prnterm "if (if zero? 0 then true else false) then true else false"
+    putStrLn $ prnterm "if false then 0 else succ (pred (succ 0))"
+    putStrLn $ prnterm "if true then true else (if false then false else false)"
+    putStrLn $ prnterm "if (zero? (if (zero? 0) then 0 else (succ 0))) then succ 0 else succ (succ 0)"
