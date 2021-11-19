ce902e45bae6aef6698ddd3c33bd170a037f4af0
veminovici <veminovici@hotmail.com>
2021-11-19 19:24:41 +0200

    Fix the eval

--- UnArithmetic/app/Main.hs
+++ UnArithmetic/app/Main.hs
@@ -4,35 +4,9 @@ import Syntax
 
 main :: IO ()
 main = do
-    let isNumerical = show . isNumericalVal 
-    let isValue = show . isVal 
-    let evaluate1 = show . eval1
-    let evaluate = show . eval
-
-    putStrLn $ "isnum zero:   " ++ isNumerical (TmZero "")
-    putStrLn $ "isnum true:   " ++ isNumerical (TmTrue "")
-    putStrLn $ "isnum false:  " ++ isNumerical (TmFalse "")
-    putStrLn $ "isnum succ n: " ++ isNumerical (TmZero "")
-    putStrLn $ "isnum succ t: " ++ isNumerical (TmTrue "")
-    putStrLn $ "isnum iszero: " ++ isNumerical (TmIsZero "" $ TmTrue "")
-
-    putStrLn $ "isval zero:   " ++ isValue (TmZero "")
-    putStrLn $ "isval true:   " ++ isValue (TmTrue "")
-    putStrLn $ "isval false:  " ++ isValue (TmFalse "")
-    putStrLn $ "isval succ n: " ++ isValue (TmZero "")
-    putStrLn $ "isval succ t: " ++ isValue (TmTrue "")
-    putStrLn $ "isval iszero: " ++ isValue (TmIsZero "" $ TmTrue "")
-
-    putStrLn $ "eval1 zero:   " ++ evaluate1 (TmZero "")
-    putStrLn $ "eval1 true:   " ++ evaluate1 (TmTrue "")
-    putStrLn $ "eval1 false:  " ++ evaluate1 (TmFalse "")
-    putStrLn $ "eval1 succ n: " ++ evaluate1 (TmZero "")
-    putStrLn $ "eval1 succ t: " ++ evaluate1 (TmTrue "")
-    putStrLn $ "eval1 iszero: " ++ evaluate1 (TmIsZero "" $ TmTrue "")
-
-    putStrLn $ "eval zero:   " ++ evaluate (TmZero "")
-    putStrLn $ "eval true:   " ++ evaluate (TmTrue "")
-    putStrLn $ "eval false:  " ++ evaluate (TmFalse "")
-    putStrLn $ "eval succ n: " ++ evaluate (TmZero "")
-    putStrLn $ "eval succ t: " ++ evaluate (TmTrue "")
-    putStrLn $ "eval iszero: " ++ evaluate (TmIsZero "" $ TmTrue "")
+    putStrLn $ "eval zero:   " ++ show (eval TmZero)
+    putStrLn $ "eval true:   " ++ show (eval TmTrue)
+    putStrLn $ "eval false:  " ++ show (eval TmFalse)
+    putStrLn $ "eval succ n: " ++ show (eval TmZero)
+    putStrLn $ "eval succ t: " ++ show (eval TmTrue)
+    putStrLn $ "eval iszero: " ++ show (eval (TmIsZero TmTrue))
--- UnArithmetic/src/Syntax.hs
+++ UnArithmetic/src/Syntax.hs
@@ -1,54 +1,51 @@
 module Syntax where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Monad
 
 -- | The extra information attached to a node (eg. source file, line number)
-type Info = String
 type Err = String
 
 data Term
-    = TmFalse Info
-    | TmTrue Info
-    | TmIf Info Term Term Term
-    | TmZero Info
-    | TmSucc Info Term
-    | TmPred Info Term
-    | TmIsZero Info Term
-    | TmException Err
+    = TmFalse
+    | TmTrue
+    | TmIf Term Term Term
+    | TmZero
+    | TmSucc Term
+    | TmPred Term
+    | TmIsZero Term
     deriving Show
 
 -- | Returns True if the term is a numerical value.
 isNumericalVal :: Term -> Bool
-isNumericalVal (TmZero _) = True
-isNumericalVal (TmSucc _ t) = isNumericalVal t
+isNumericalVal TmZero = True
+isNumericalVal (TmSucc t) = isNumericalVal t
 isNumericalVal _ = False
 
 -- | Returns True if the term is a value.
 isVal :: Term -> Bool
-isVal (TmFalse _) = True
-isVal (TmTrue _) = True
+isVal TmFalse = True
+isVal TmTrue = True
 isVal t | isNumericalVal t = True
 isVal _ = False
 
-dummyInfo = "" :: Info
-
 -- | One-step evaluation for a term
-eval1 :: Term -> Term
-eval1 (TmIf _ (TmTrue _) t1 _) = t1
-eval1 (TmIf _ (TmFalse _) _ t2) = t2
-eval1 (TmIf i t1 t2 t3) = let t1' = eval1 t1 in TmIf i t1' t2 t3
-eval1 (TmSucc i t1) = let t1' = eval1 t1 in TmSucc i t1'
-eval1 (TmPred _ (TmZero _)) = TmZero dummyInfo
-eval1 (TmPred _ (TmSucc _ t)) | isNumericalVal t = t
-eval1 (TmPred i t1) = let t1' = eval1 t1 in TmPred i t1'
-eval1 (TmIsZero _ (TmZero _)) = TmTrue dummyInfo
-eval1 (TmIsZero _ (TmSucc _ t)) | isNumericalVal t = TmFalse dummyInfo
-eval1 (TmIsZero i t1) = let t1' = eval1 t1 in TmIsZero i t1'
-eval1 _ = TmException "No rule applies"
+eval1 :: Term -> Maybe Term
+eval1 TmTrue = Nothing
+eval1 TmFalse = Nothing
+eval1 (TmIf TmTrue  t1 _) = return t1
+eval1 (TmIf TmFalse _ t2) = return t2
+eval1 (TmIf t1 t2 t3) = liftM3 TmIf (eval1 t1) (return t2) (return t3)
+eval1 TmZero = Nothing
+eval1 (TmSucc t1) = fmap TmSucc (eval1 t1)
+eval1 (TmPred TmZero) = Just TmZero
+eval1 (TmPred (TmSucc t)) | isNumericalVal t = Just t
+eval1 (TmPred t1) = fmap TmPred (eval1 t1)
+eval1 (TmIsZero TmZero) = Just TmTrue
+eval1 (TmIsZero (TmSucc t)) | isNumericalVal t = Just TmFalse
+eval1 (TmIsZero t1) = fmap TmIsZero (eval1 t1)
 
 -- | Full evaluation
 eval :: Term -> Term
-eval t@(TmException _) = t
-eval t = let t' = eval1 t in eval t'
+eval t =
+    maybe t eval (eval1 t)
 
