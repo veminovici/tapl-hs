01282c86fb4e4679e7de4ac4be66547eccf8b8ae
veminovici <veminovici@hotmail.com>
2021-11-24 14:41:58 +0200

    Add more functions based on reviewing the ch 3 and 4

--- UnArithmetic/README.md
+++ UnArithmetic/README.md
@@ -1,1 +1,3 @@
-# UnArithmetic
+# Simplee::Tapl::Unarithmetic
+
+This is an implementation of an untyped arithmetic language. It is based on the chapters 3 and 4 from the [Types and Programming Languages](http://www.cis.upenn.edu/~bcpierce/tapl/).
--- UnArithmetic/src/Syntax.hs
+++ UnArithmetic/src/Syntax.hs
@@ -1,51 +1,74 @@
-module Syntax (
-    eval,
-    eval1,
-    Term(..) ) where
+module Syntax
+    ( Term(..)
+    , eval1
+    , eval
+    ) where
 
 import Control.Monad
 
-data Term
-    = TmFalse               -- | false value
-    | TmTrue                -- | true value
-    | TmIf Term Term Term   -- | if t1 then t2 else t3
-    | TmZero                -- | zero value
-    | TmSucc Term           -- | successor
-    | TmPred Term           -- | predecesor
-    | TmIsZero Term         -- | iszero t
-    deriving Show
+data Term                   -- | Terminals
+    = TmFalse               -- | FALSE value
+    | TmTrue                -- | TRUE value
+    | TmIf Term Term Term   -- | IF expression
+    | TmZero                -- | ZERO value
+    | TmSucc Term           -- | Successor
+    | TmPred Term           -- | Predecessor
+    | TmIsZero Term         -- | Zero test
+    deriving (Show)
 
--- | Returns True if the term is a numerical value.
-isNumericalVal :: Term -> Bool
-isNumericalVal TmZero = True
-isNumericalVal (TmSucc t) = isNumericalVal t
-isNumericalVal _ = False
+-- | Returns the number of constants in a given term
+consts :: Term -> Int
+consts TmFalse = 1
+consts TmTrue = 1
+consts (TmIf t1 t2 t3) = consts t1 + consts t2 + consts t3
+consts TmZero = 1
+consts (TmSucc t) = consts t
+consts (TmPred t) = consts t
+consts (TmIsZero t) = consts t
 
--- | Returns True if the term is a value.
-isVal :: Term -> Bool
-isVal TmFalse = True
-isVal TmTrue = True
-isVal t | isNumericalVal t = True
-isVal _ = False
+-- | Returns the size (total number of sub-terms) for a given term
+size :: Term -> Int
+size TmFalse = 1
+size TmTrue = 1
+size (TmIf t1 t2 t3) = 1 + size t1 + size t2 + size t3
+size TmZero = 1
+size (TmSucc t) = 1 + size t
+size (TmPred t) = 1 + size t
+size (TmIsZero t) =  1 + size t
 
--- | One-step evaluation for a term
+-- | Returns the depth of a term.
+depth :: Term -> Int
+depth TmFalse = 1
+depth TmTrue = 1
+depth (TmIf t1 t2 t3) = let xs = map depth [t1, t2, t3] in maximum xs
+depth TmZero = 1
+depth (TmSucc t) = 1 + depth t
+depth (TmPred t) = 1 + depth t
+depth (TmIsZero t) = 1 + depth t
+
+-- | Returns true if a given term is a numeric value
+isNumericalValue :: Term -> Bool
+isNumericalValue TmZero = True
+isNumericalValue (TmSucc t) = isNumericalValue t
+isNumericalValue _ = False
+
+-- | Returns the new term based on the one-step evaludation of a given term.
+-- If the given term cannot be evaluated (normal form), it returns Nothing.
 eval1 :: Term -> Maybe Term
-eval1 TmTrue = Nothing
 eval1 TmFalse = Nothing
-eval1 (TmIf TmTrue  t1 _) = return t1
-eval1 (TmIf TmFalse _ t2) = return t2
+eval1 TmTrue = Nothing
+eval1 (TmIf TmTrue t1 _) = Just t1
+eval1 (TmIf TmFalse _ t2) = Just t2
 eval1 (TmIf t1 t2 t3) = liftM3 TmIf (eval1 t1) (return t2) (return t3)
 eval1 TmZero = Nothing
-eval1 (TmSucc t1) = fmap TmSucc (eval1 t1)
+eval1 (TmSucc t) = fmap TmSucc (eval1 t)
 eval1 (TmPred TmZero) = Just TmZero
-eval1 (TmPred (TmSucc t)) | isNumericalVal t = Just t
-eval1 (TmPred t1) = fmap TmPred (eval1 t1)
-eval1 (TmIsZero TmZero) = Just TmTrue
-eval1 (TmIsZero (TmSucc t)) | isNumericalVal t = Just TmFalse
-eval1 (TmIsZero t1) = fmap TmIsZero (eval1 t1)
+eval1 (TmPred (TmSucc t)) | isNumericalValue t = Just t
+eval1 (TmPred t) = fmap TmPred (eval1 t)
+eval1 (TmIsZero TmZero) = Just TmZero
+eval1 (TmIsZero (TmSucc t)) | isNumericalValue t = Just t
+eval1 (TmIsZero t) = fmap TmIsZero (eval1 t)
 
--- | Full evaluation
+-- | Runs the multi-step evaluation for a given tem
 eval :: Term -> Term
-eval t =
-    maybe t eval (eval1 t)
-
+eval t = maybe t eval (eval1 t)
